fba60efb404fadad3f73021f49808d61aa32b182
santhosh <santhosh.sivaraman@onedatasoftware.com>
2024-03-19 15:40:34 +0530

    add authenticated

--- app/views.py
+++ app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from app.models import Student
 from app.serializers import Studentserializer
 from rest_framework import viewsets
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated
 
 class StudentViewsets(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = Studentserializer
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
